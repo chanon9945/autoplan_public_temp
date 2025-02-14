32229d2fa2e5df26ed2a0b3eb677817f69a8859d
mono <krittanat.tat@student.mahidol.edu>
2025-02-14 14:22:18 +0700

    label side,level

--- BARTPedicleScrewSimulatorWizard/LandmarksStep.py
+++ BARTPedicleScrewSimulatorWizard/LandmarksStep.py
@@ -20,48 +20,14 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
       self.fiducialNodeObservations = []
       self.fidMoveObserverTag = None
 
-
     def killButton(self):
       # hide useless button
       bl = slicer.util.findChildren(text='Final')
       if len(bl):
         bl[0].hide()
 
-    def begin(self):
-      # TODO: we could prepare placement mode here
-      pass
-
     def stop(self):
       self.startMeasurements.placeModeEnabled = False
-
-    def cameraFocus(self, position):
-      camera = slicer.mrmlScene.GetNodeByID('vtkMRMLCameraNode1')
-
-      if self.approach == 'Posterior':
-
-          camera.SetFocalPoint(*position)
-          camera.SetPosition(position[0],-200,position[2])
-          camera.SetViewUp([0,0,1])
-
-      elif self.approach == 'Anterior':
-
-          camera.SetFocalPoint(*position)
-          camera.SetPosition(position[0],200,position[2])
-          camera.SetViewUp([0,0,1])
-
-      elif self.approach == 'Left':
-
-          camera.SetFocalPoint(*position)
-          camera.SetPosition(-200,position[1],position[2])
-          camera.SetViewUp([0,0,1])
-
-      elif self.approach == 'Right':
-
-          camera.SetFocalPoint(*position)
-          camera.SetPosition(200,position[1],position[2])
-          camera.SetViewUp([0,0,1])
-
-      camera.ResetClippingRange()
 
     def onTableCellClicked(self):
       if self.table2.currentColumn() == 0:
@@ -70,9 +36,36 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
           position = [0,0,0]
           self.fiducial = self.fiducialNode()
           self.fiducial.GetNthControlPointPosition(currentFid,position)
-          logging.debug(position)
-          # self.cameraFocus(position)
+          self.zoomIn()
+          self.sliceChange()
 
+    def sliceChange(self):
+        logging.debug("changing")
+        currentFid = self.table2.currentRow()
+        coords = [0,0,0]
+        if self.fiducial != None:
+          self.fiducial.GetNthControlPointPosition(currentFid,coords)
+          slicer.modules.markups.logic().JumpSlicesToLocation(coords[0], coords[1], coords[2], True)
+        else:
+            return
+
+    def zoomIn(self):
+      logging.debug("zoom")
+      slicer.app.applicationLogic().PropagateVolumeSelection(1)
+      # Get the slice logic for Red
+      sliceWidget = slicer.app.layoutManager().sliceWidget("Red")
+      sliceLogic = sliceWidget.sliceLogic()
+      sliceNode = sliceLogic.GetSliceNode()
+
+      # Get current FOV
+      fov = sliceNode.GetFieldOfView()
+
+      # Reduce FOV by 20% to zoom in
+      newFov = [fov[0] * 0.3, fov[1] * 0.3, fov[2]]
+      sliceNode.SetFieldOfView(newFov[0], newFov[1], newFov[2])
+
+      # Update slice matrices to apply changes
+      sliceNode.UpdateMatrices()
 
     def updateTable(self):
         self.fiducial = self.fiducialNode()
@@ -144,14 +137,129 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
                 self.table2.removeRow(row)
 
     def lockFiducials(self):
-      fidNode = self.fiducialNode()
-      slicer.modules.markups.logic().SetAllControlPointsLocked(fidNode,True)
+        fidNode = self.fiducialNode()
+        slicer.modules.markups.logic().SetAllControlPointsLocked(fidNode,True)
 
-    def addFiducials(self):
-      pass
+    def getSegmentationMidlineX(self, segNode):
+        rasBounds = [0] * 6
+        segNode.GetRASBounds(rasBounds)  # [xmin, xmax, ymin, ymax, zmin, zmax]
+        centerX = 0.5 * (rasBounds[0] + rasBounds[1])  # average of xmin and xmax
+        return centerX
 
     def addFiducialToTable(self, observer, event):
       self.updateTable()
+
+      # Assume that the new fiducial is the last one added.
+      fidNumber = self.fiducial.GetNumberOfDefinedControlPoints()
+      if fidNumber == 0:
+          return
+
+      fidIndex = fidNumber - 1
+      position = [0, 0, 0]
+      self.fiducial.GetNthControlPointPosition(fidIndex, position)
+
+      # Get the segmentation node
+      try:
+          segNode = slicer.util.getNode("Segmentation")
+      except Exception as e:
+          # logging.error("Segmentation node not found: " + str(e))
+          return
+
+      # detect side from the segmentation
+      centerX = self.getSegmentationMidlineX(segNode)
+      if position[0] < centerX:
+          detectedSide = "Left"
+      else:
+          detectedSide = "Right"
+
+      # Update the side combo box in the table
+      sideComboBox = self.table2.cellWidget(fidIndex, 2)
+      if sideComboBox:
+          sideComboBox.setCurrentText(detectedSide)
+
+      # Store the side as an attribute on the fiducial node
+      fidLabel = self.fiducial.GetNthControlPointLabel(fidIndex)
+      self.fiducial.SetAttribute(f"{fidLabel}_Side", detectedSide)
+
+      detectedLevel = self.getLumbarLevelAtPosition(position, segNode)
+      if detectedLevel:
+          # Update the combo box for the level column
+          comboLevel = self.table2.cellWidget(fidIndex, 1)
+          if comboLevel:
+              comboLevel.setCurrentText(detectedLevel)
+          # Also store the detected level as an attribute in the fiducial node
+          fidLabel = self.fiducial.GetNthControlPointLabel(fidIndex)
+          self.fiducial.SetAttribute(f"{fidLabel}_Level", detectedLevel)
+
+    def getLumbarLevelAtPosition(self, position, segNode):
+        """
+        Given a world (RAS) position and a segmentation node,
+        converts the point into voxel coordinates,
+        and checks if the point lies inside the segment.
+        """
+        segmentation = segNode.GetSegmentation()
+        segmentIDs = segmentation.GetSegmentIDs()
+
+        for segId in segmentIDs:
+            # Create a labelmap node to hold the exported segment
+            labelmapNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLLabelMapVolumeNode")
+
+            success = slicer.modules.segmentations.logic().ExportSegmentsToLabelmapNode(
+                segNode,
+                [segId],
+                labelmapNode
+            )
+            if not success:
+                slicer.mrmlScene.RemoveNode(labelmapNode)
+                continue
+
+            # Get the RAS->IJK transform
+            rasToIjkMatrix = vtk.vtkMatrix4x4()
+            labelmapNode.GetRASToIJKMatrix(rasToIjkMatrix)
+
+            # Convert the RAS point to IJK coordinates
+            rasPoint = [position[0], position[1], position[2], 1]
+            ijkPoint = [0, 0, 0, 0]
+            rasToIjkMatrix.MultiplyPoint(rasPoint, ijkPoint)
+
+            # Round to the nearest voxel index
+            ijkIndices = [int(round(ijkPoint[i])) for i in range(3)]
+
+            # Get the actual voxel data from the labelmap node
+            labelmapData = labelmapNode.GetImageData()
+            if not labelmapData:
+                slicer.mrmlScene.RemoveNode(labelmapNode)
+                continue
+
+            dims = labelmapData.GetDimensions()
+            # Check index bounds
+            if (ijkIndices[0] < 0 or ijkIndices[0] >= dims[0] or
+                    ijkIndices[1] < 0 or ijkIndices[1] >= dims[1] or
+                    ijkIndices[2] < 0 or ijkIndices[2] >= dims[2]):
+                slicer.mrmlScene.RemoveNode(labelmapNode)
+                continue
+
+            # Check the voxel value at (i, j, k). If > 0, point is inside the segment
+            voxelValue = labelmapData.GetScalarComponentAsDouble(
+                ijkIndices[0],
+                ijkIndices[1],
+                ijkIndices[2],
+                0
+            )
+            if voxelValue > 0:
+                # Found a segment containing the point
+                segment = segmentation.GetSegment(segId)
+                if segment:
+                    segName = segment.GetName()
+                    detectedLevel = segName.split()[0]
+                    if detectedLevel in self.levelselection:
+                        slicer.mrmlScene.RemoveNode(labelmapNode)
+                        return detectedLevel
+
+            # Remove the temporary labelmap node to keep the scene clean
+            slicer.mrmlScene.RemoveNode(labelmapNode)
+
+        return None
 
     def createUserInterface( self ):
       markup = slicer.modules.markups.logic()
@@ -179,12 +287,11 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
                   "background-color: red;"
               )
 
-      # Initialize the button style to inactive (red)
+      # Initialize the button style to inactive
       updateButtonStyle(False)
 
       # Connect to the same signal that indicates a change in the active placing mode
       self.startMeasurements.connect('activeMarkupsFiducialPlaceModeChanged(bool)', updateButtonStyle)
-      self.startMeasurements.connect('activeMarkupsFiducialPlaceModeChanged(bool)', self.addFiducials)
 
       buttonLayout = qt.QHBoxLayout()
       buttonLayout.addWidget(self.startMeasurements)
@@ -217,22 +324,15 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
       self.__layout.addWidget(self.deleteFid)
 
       # Camera Transform Sliders
-
       transCam = ctk.ctkCollapsibleButton()
       transCam.text = "Shift Camera Position"
       transCam.collapsed = True
       self.__layout.addWidget(transCam)
-      #transCam.collapsed = True
       camLayout = qt.QFormLayout(transCam)
 
       a = PythonQt.qMRMLWidgets.qMRMLTransformSliders()
       a.setMRMLTransformNode(slicer.mrmlScene.GetNodeByID('vtkMRMLLinearTransformNode4'))
-      #transWidget = slicer.modules.transforms.createNewWidgetRepresentation()
-      #transSelector = transWidget.findChild('qMRMLNodeComboBox')
-      #transWidgetPart = transWidget.findChild('ctkCollapsibleButton')
-      #transformSliders = transWidgetPart.findChildren('qMRMLTransformSliders')
       camLayout.addRow(a)
-
 
       qt.QTimer.singleShot(0, self.killButton)
 
@@ -240,7 +340,6 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
     def onEntry(self, comingFrom, transitionType):
 
       super(LandmarksStep, self).onEntry(comingFrom, transitionType)
-
       qt.QTimer.singleShot(0, self.killButton)
 
       lm = slicer.app.layoutManager()
@@ -251,36 +350,55 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
       pNode = self.parameterNode()
       logging.debug(pNode)
       self.levelselection = ["L1", "L2", "L3", "L4", "L5"]
-      # self.vertebra = str(pNode.GetParameter('vertebra'))
-      # self.inst_length = str(pNode.GetParameter('inst_length'))
-      # self.approach = str(pNode.GetParameter('approach'))
-      # for i in range(self.levels.index(self.vertebra),self.levels.index(self.vertebra)+int(self.inst_length)):
-      #     #logging.debug(self.levels[i])
-      #     self.levelselection.append(self.levels[i])
-      # logging.debug(self.levelselection)
 
       camera = slicer.mrmlScene.GetNodeByID('vtkMRMLCameraNode1')
       camera.SetPosition(0, -600, 0)
       camera.SetViewUp([0, 0, 1])
-      # if self.approach == 'Posterior':
-      #     logging.debug("posterior")
-      #     camera.SetPosition(0,-600,0)
-      #     camera.SetViewUp([0,0,1])
-      # elif self.approach == 'Anterior':
-      #     logging.debug("Anterior")
-      #     camera.SetPosition(0,600,0)
-      #     camera.SetViewUp([0,0,1])
-      # elif self.approach == 'Left':
-      #     logging.debug("Left")
-      #     camera.SetPosition(-600,0,0)
-      #     camera.SetViewUp([0,0,1])
-      # elif self.approach == 'Right':
-      #     logging.debug("Right")
-      #     camera.SetPosition(600,0,0)
-      #     camera.SetViewUp([0,0,1])
       camera.ResetClippingRange()
-      #pNode = self.parameterNode()
-      #pNode.SetParameter('currentStep', self.stepid)
+
+      # Get the segmentation node
+      try:
+          segNode = slicer.util.getNode("Segmentation")
+      except Exception as e:
+          logging.error("Segmentation node not found: " + str(e))
+          segNode = None
+
+      if segNode:
+          segmentation = segNode.GetSegmentation()
+          segmentIDs = segmentation.GetSegmentIDs()
+          # For demonstration, we use the first segment.
+          if segmentIDs:
+              segId = segmentIDs[0]
+              labelmapNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLLabelMapVolumeNode")
+              success = slicer.modules.segmentations.logic().ExportSegmentsToLabelmapNode(
+                  segNode,
+                  [segId],
+                  labelmapNode
+              )
+              if success:
+                  # Now, get the cropped bounds from the labelmap data.
+                  labelmapData = labelmapNode.GetImageData()
+                  if labelmapData:
+                      rasBounds = [0] * 6
+                      labelmapNode.GetRASBounds(rasBounds)
+                      # Use these bounds to compute the first slice location
+                      firstSliceLocation = [
+                          0.5 * (rasBounds[0] + rasBounds[1]),  # x-center
+                          0.5 * (rasBounds[2] + rasBounds[3]),  # y-center
+                          0.9 * rasBounds[5]
+                      ]
+                      slicer.modules.markups.logic().JumpSlicesToLocation(
+                          firstSliceLocation[0],
+                          firstSliceLocation[1],
+                          firstSliceLocation[2],
+                          True
+                      )
+                  else:
+                      logging.error("Labelmap image data not found.")
+              else:
+                  logging.error("Failed to export segment to labelmap.")
+              # Clean up the temporary labelmap node when done
+              slicer.mrmlScene.RemoveNode(labelmapNode)
 
       fiducialNode = self.fiducialNode()
       self.startMeasurements.setCurrentNode(fiducialNode)
@@ -302,10 +420,6 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
             fiducialNode.RemoveObserver(observation)
           self.fiducialNodeObservations = []
           self.doStepProcessing()
-          #logging.debug(self.table2.cellWidget(0,1).currentText)
-
-      #if goingTo.id() == 'Threshold':
-          #slicer.mrmlScene.RemoveNode(self.__outModel)
 
       if goingTo.id() != 'Segmentation' and goingTo.id() != 'Measurements':
           return
@@ -317,23 +431,13 @@ class LandmarksStep( PedicleScrewSimulatorStep ):
       self.__parent.validate( desiredBranchId )
       self.__parent.validationSucceeded(desiredBranchId)
 
-      #self.inputFiducialsNodeSelector.update()
-      #fid = self.inputFiducialsNodeSelector.currentNode()
       fidNumber = self.fiducial.GetNumberOfDefinedControlPoints()
 
-      #pNode = self.parameterNode()
       if fidNumber != 0:
-      #  fidID = fid.GetID()
-      #  if fidID != '':
-      #    pNode = self.parameterNode()
-      #    pNode.SetNodeReferenceID('fiducial', fidID)
           self.__parent.validationSucceeded(desiredBranchId)
       else:
           self.__parent.validationFailed(desiredBranchId, 'Error','Please place at least one fiducial on the model before proceeding')
 
     def doStepProcessing(self):
-      #list = ['a','b','c']
-      #listNode = self.parameterNode()
-      #listNode.SetParameter = ('list', list)
       logging.debug('Done')
       self.lockFiducials()
