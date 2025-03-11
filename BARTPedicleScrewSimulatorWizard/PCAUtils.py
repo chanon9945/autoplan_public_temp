936db46fabe16db88e3d9b126c92020c88a749f0
chanon9945 <chanon.issara@gmail.com>
2025-03-11 14:59:54 +0700

    fix pca

--- BARTPedicleScrewSimulatorWizard/PCAUtils.py
+++ BARTPedicleScrewSimulatorWizard/PCAUtils.py
@@ -2,3 +2,12 @@ import numpy as np
 import scipy as sp
 from sklearn.decomposition import PCA
 
+def apply_pca(volume):
+    pca = PCA(n_components=3)
+    pca.fit(volume)
+    coeff = pca.components_
+    latent = pca.explained_variance_ratio_
+    score = pca.transform(volume)
+    return coeff,latent,score
+
+    
